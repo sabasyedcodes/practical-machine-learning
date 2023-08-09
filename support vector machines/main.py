b9279fd5dcd9375a8acf188c0bd6c9d98f98991f
sabasyedcodes <sabaa9018@gmail.com>
2023-08-09 18:47:18 +0530

    adding support vector machine files

---
+++ support vector machines/main.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
+data = load_breast_cancer()
+
+x = data.data
+y = data.target
+
+x_train , x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=23)
+
+#linear is fastest
+#c is soft margin
+clf = SVC(kernel='linear',C=3)
+clf.fit(x_train,y_train)
+
+clf2 =KNeighborsClassifier(n_neighbors=3)
+clf2.fit(x_train,y_train)
+print(f'SVC: {clf.score(x_test,y_test)}')
+print(f'KNN: {clf2.score(x_test,y_test)}')
+
+# if we want same results in both 
+# support vector machine and kNeighbors
+# we set random state=23 i.e shuffling
